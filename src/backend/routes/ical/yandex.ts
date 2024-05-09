3ece15350d3ffcb596dbb2724922a4d78da01f96
Mark S. <127447378+voidsMark@users.noreply.github.com>
2024-05-09 22:28:00 +0500

    small fix

--- src/backend/routes/ical/yandex.ts
+++ src/backend/routes/ical/yandex.ts
@@ -4,7 +4,7 @@ import { fixYandexICal } from '@/backend/utils/ical'
 
 export default async (fastify: FastifyInstance) => {
   /** Get iCal from yandex calendar */
-  fastify.get('/yandex', async (req, reply) => {
+  fastify.get('/yandex/calendar.ics', async (req, reply) => {
     const iCal = await getYandexICal()
     const fixedICal = fixYandexICal(iCal)
 
